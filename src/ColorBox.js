630aff73a243e5b7b8a6d2b206574ad47b527a32
roma <mr.anadale@yahoo.com>
2019-02-18 13:18:52 -0500

    I love recursion

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
-export default class ColorBox extends Component {
-  
+class ColorBox extends Component {
   render() {
-    return (
-      <div className="color-box" style={{opacity: 2}}>
-        {/* your conditional code here! */}
-      </div>
-    )
+    if (this.props.opacity >= 0.2) {
+      const newOpacity = (this.props.opacity * 10 - 0.1 * 10) / 10
+      console.log(newOpacity)
+      return (
+        <div className="color-box" style={{ opacity: this.props.opacity }}>
+          <ColorBox opacity={newOpacity} />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
-  
 }
+
+export default ColorBox
