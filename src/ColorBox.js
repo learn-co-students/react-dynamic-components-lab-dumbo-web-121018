59f69d25972dd4c132497e75bbdb17d300c863fc
Roman Pirag <mr.anadale@yahoo.com>
2019-02-18 18:20:46 +0000

    Done.

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,7 +4,6 @@ class ColorBox extends Component {
   render() {
     if (this.props.opacity >= 0.2) {
       const newOpacity = (this.props.opacity * 10 - 0.1 * 10) / 10
-      console.log(newOpacity)
       return (
         <div className="color-box" style={{ opacity: this.props.opacity }}>
           <ColorBox opacity={newOpacity} />
